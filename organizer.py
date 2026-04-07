cbcce20a65c914106e7a2b15bb33b365498e0f77
sakthi-source <mailtosakthivelb@gmail.com>
2026-04-07 12:10:41 +0530

    Add files via upload

---
+++ organizer.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+
+folder_path = "test_folder"
+
+files = os.listdir(folder_path)
+
+for file in files:
+    file_path = os.path.join(folder_path, file)
+
+    if os.path.isfile(file_path):
+        if file.endswith(".jpg") or file.endswith(".png"):
+            os.makedirs(folder_path + "/Images", exist_ok=True)
+            shutil.move(file_path, folder_path + "/Images/" + file)
+
+        elif file.endswith(".pdf") or file.endswith(".txt"):
+            os.makedirs(folder_path + "/Documents", exist_ok=True)
+            shutil.move(file_path, folder_path + "/Documents/" + file)
+
+        elif file.endswith(".mp3"):
+            os.makedirs(folder_path + "/Music", exist_ok=True)
+            shutil.move(file_path, folder_path + "/Music/" + file)
+
+print("Files organized successfully")
